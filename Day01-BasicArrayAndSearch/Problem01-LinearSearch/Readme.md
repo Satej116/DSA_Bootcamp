f860927d418d2fdf43474d397cc0674ea26c0bb9
Satej Kulkarni <129601500+Satej116@users.noreply.github.com>
2025-05-29 06:47:50 +0000

    Added Day 1 - Problem01 = Linear Search

--- Day01-BasicArrayAndSearch/Problem01-LinearSearch/Readme.md
+++ Day01-BasicArrayAndSearch/Problem01-LinearSearch/Readme.md
@@ -1,6 +1,7 @@
 This is a simple Python program that takes a list of numerical values from the user, then searches for a target value within that list. If the target value is found, the program prints the index of the element. If not found, it prints -1.
 
 Features
+
 Takes the size of the array as input from the user.
 
 Accepts only numerical inputs to populate the array.
--- Day01-BasicArrayAndSearch/Problem01-LinearSearch/solution.py
+++ Day01-BasicArrayAndSearch/Problem01-LinearSearch/solution.py
@@ -2,10 +2,12 @@ l = int(input("Enter the Length of Array= "))
 count = 0
 arr = []
 print("Enter only numberical values")
+
 for i in range (0,l):
     val = int(input())
     arr.append(val)
 print(arr)
+
 target = int(input("Enter the Target Value = "))
 
 for j in range(0,l):
@@ -16,4 +18,4 @@ for j in range(0,l):
         count = j
 
 if(count == l-1):
-    print("-1")
+    print("Index = -1")
