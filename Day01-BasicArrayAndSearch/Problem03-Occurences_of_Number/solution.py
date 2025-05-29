ba3346ac8c2cc778a3fcdcdf7e37e01f4e932d0c
Satej Kulkarni <129601500+Satej116@users.noreply.github.com>
2025-05-29 13:12:36 +0000

    some changes done in problem03

--- Day01-BasicArrayAndSearch/Problem03-Occurences_of_Number/solution.py
+++ Day01-BasicArrayAndSearch/Problem03-Occurences_of_Number/solution.py
@@ -1,36 +1,39 @@
 def GetData():
     arr = []
-    try:
-        while True:
-            arr_length = int(input("Enter the Length of Array = "))
-            if arr_length > 0:
-                try:
-                    print("Enter elements in the array:\n")
-                    for i in range(arr_length):
-                        val = int(input())
-                        arr.append(val)
-                    print("Array =", arr)
-                    break
-                except:
-                    print("Enter Numerical values only")
-            else:
-                print("Length must be a positive number. Please try again.")
+    while True:
+        try:
+                arr_length = int(input("Enter the Length of Array = "))
+                if arr_length > 0:
+                    try:
+                        print("Enter elements in the array:\n")
+                        for i in range(arr_length):
+                            val = int(input())
+                            arr.append(val)
+                        print("Array =", arr)
+                        break
+                    except:
+                        print("Enter Numerical values only")
+                else:
+                    print("Length must be a positive number. Please try again.")
             
-        return arr, arr_length
-
-    except:
-        print("Enter only numerical values")
+        except:
+            print("Enter only numerical values")
+    return arr, arr_length
         
 def Occurences_of_Number(arr, arr_length):
-        
-    target_value = int(input("Enter the Target Value = "))
-    count = 0
+    while True:
+        try:     
+            target_value = int(input("Enter the Target Value = "))
+            count = 0
     
-    for j in range(0,arr_length):
-        if target_value == arr[j]:
-            count = count + 1
-    
-    print (count)
+            for j in range(0,arr_length):
+                if target_value == arr[j]:
+                    count = count + 1
+
+            print (count)
+            break
+        except:
+            print("Enter only numerical value")
 
 arr, arr_length = GetData()
 Occurences_of_Number(arr, arr_length)
