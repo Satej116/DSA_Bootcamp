fab84c28bc97ebd5368909f282c0796e663e419c
Satej Kulkarni <129601500+Satej116@users.noreply.github.com>
2025-05-29 09:59:37 +0000

    Changes not of negative array length input

--- Day01-BasicArrayAndSearch/Problem01-LinearSearch/solution.py
+++ Day01-BasicArrayAndSearch/Problem01-LinearSearch/solution.py
@@ -1,25 +1,27 @@
-arr_length = int(input("Enter the Length of Array= "))
 arr = []
 
 try:
-    if(arr_length>1):
-        for i in range (0, arr_length):
-            val = int(input())
-            arr.append(val)
-        print(arr)
-    else:
-        print("Length cannot be negative")
+    while True:
+        arr_length = int(input("Enter the Length of Array = "))
+        if arr_length > 0:
+            break
+        else:
+            print("Length must be a positive number. Please try again.")
+
+    print("Enter elements in the array:\n")
+    for i in range(arr_length):
+        val = int(input())
+        arr.append(val)
+    print("Array =", arr)
 
     target_value = int(input("Enter the Target Value = "))
     
-    for j in range(0, arr_length):
-        if(target_value == arr[j]):
-            print("Index = ",j)
+    for j in range(arr_length):
+        if target_value == arr[j]:
+            print("Index =", j)
             break
     else:
         print("Index = -1")
 
-
-except:
-    print("Enter only numberical values")
-
+except ValueError:
+    print("Enter only numerical values")
--- Day01-BasicArrayAndSearch/Problem02-BinarySearch/solution.py
+++ Day01-BasicArrayAndSearch/Problem02-BinarySearch/solution.py
@@ -1,33 +1,37 @@
-l = int(input("Enter the Length of Array= "))
 arr = []
 
-
 try:
-    for i in range (0,l):
-        val = int(input())
+    while True:
+        l = int(input("Enter the Length of Array = "))
+        if l > 0:
+            break
+        else:
+            print("Length must be a positive number. Please try again.")
+
+    print("Enter the elements:")
+    for i in range(l):
+        val = int(input(f"Element {i+1}: "))
         arr.append(val)
 
     arr.sort()
-    print("Sorted array:",arr)
+    print("Sorted array:", arr)
 
     target = int(input("Enter the Target Value = "))
 
     start = 0
     end = len(arr) - 1
 
-
     while start <= end:
-        mid = (start + end)//2
-        if (target == arr[mid]):
-            print(mid)
+        mid = (start + end) // 2
+        if target == arr[mid]:
+            print("Index =", mid)
             break
-        elif(target > arr[mid]):
+        elif target > arr[mid]:
             start = mid + 1
         else:
             end = mid - 1
-        
     else:
         print("Index = -1")
 
-except:
-    print("Enter only numberical values")
+except ValueError:
+    print("Enter only numerical values.")
