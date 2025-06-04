10c65e77b0f255d6cd84d20cebb1559430a9026a
Satej Kulkarni <129601500+Satej116@users.noreply.github.com>
2025-06-04 16:04:29 +0000

    Some changes done

--- Day06_String_Basics/Problem03_Anagrams/solution.py
+++ Day06_String_Basics/Problem03_Anagrams/solution.py
@@ -14,9 +14,9 @@ def Anagrams(s1,s2):
     if(len(s1)==len(s2)):
 
         for j in range(len(s1)):
-                for k in range(len(s1)-j-1):
-                    if (s1[k]>s1[k+1]):
-                        s1[k], s1[k + 1] = s1[k + 1], s1[k]
+            for k in range(len(s1)-j-1):
+                if (s1[k]>s1[k+1]):
+                    s1[k], s1[k + 1] = s1[k + 1], s1[k]
         
         for j in range(len(s2)):
             for k in range(len(s2)-j-1):
@@ -27,6 +27,9 @@ def Anagrams(s1,s2):
             print (True)
         else:
             print (False)
+    
+    else:
+        print(False)
 
 s1,s2= GetData()
 Anagrams(s1,s2)
